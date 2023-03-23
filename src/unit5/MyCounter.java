7a20bcb392fecdc40ab923d962873ce6f2094ad0
d556f8 <l.juhyeonni@gmail.com>
2023-03-23 11:12:26 +0900

    UPDATE: new methods

--- src/unit5/MyCounter.java
+++ src/unit5/MyCounter.java
@@ -1,15 +1,40 @@
 class MyCounter {
-    int counter;
+    private int value;
+
     MyCounter(int value) {
-        counter = value;
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    // 파라미터로 들어온 인스턴스(MyCounter 형식)의 value 값을 증가시킨다.
+    public static void increase(MyCounter obj) {
+        int value = obj.getValue() + 1;
+        obj.setValue(value);
+    }
+
+    @Override
+    public String toString() {
+        return "MyCounter{" +
+                "value=" + value +
+                '}';
     }
 }
 
 class Test {
     public static void main(String[] args) {
         MyCounter obj1 = new MyCounter(100);
-        MyCounter obj2 = new MyCounter(200);
-        System.out.println("객체 1의 counter = " + obj1.counter);
-        System.out.println("객체 2의 counter = " + obj2.counter);
+        System.out.println(obj1);
+
+        // static 메소드를 사용하여 value 값 증가
+        MyCounter.increase(obj1);
+
+        System.out.println(obj1);
     }
 }
