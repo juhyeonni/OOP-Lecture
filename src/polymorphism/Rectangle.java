3aea18111a6d94989f0d4dfb16fa9cda7d3758b4
d556f8 <l.juhyeonni@gmail.com>
2023-04-10 14:53:09 +0900

    Feat: add `polymorphism` related class content

---
+++ src/polymorphism/Rectangle.java
@@ -0,0 +1,29 @@
+package polymorphism;
+
+public class Rectangle extends Shape {
+
+  private int width;
+
+  public int getWidth() {
+    return width;
+  }
+
+  public void setWidth(int width) {
+    this.width = width;
+  }
+
+  public int getHeight() {
+    return height;
+  }
+
+  public void setHeight(int height) {
+    this.height = height;
+  }
+
+  private int height;
+
+  @Override
+  public void draw() {
+    System.out.println("Rectangle Draw[" + width + "," + height + "]");
+  }
+}
